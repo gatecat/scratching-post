3f7c2495399665399929926d74a8eed32267285c
gatecat <gatecat@ds0.me>
2022-01-12 13:46:07 +0000

    Adding openrcx import
    Signed-off-by: gatecat <gatecat@ds0.me>

---
+++ sta/openrcx.tcl
@@ -0,0 +1,4 @@
+read_lef export/StdCellLib.lef
+read_def export/corona_cts_export.def
+extract_parasitics -ext_model_file ../../open_pdks/sky130/sky130A/libs.tech/openlane/rcx_rules.info
+write_spef export/corona_cts_export.spef
