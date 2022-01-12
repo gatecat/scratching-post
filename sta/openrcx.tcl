4c4c253dd1b08aa0ba4807fd2eff6d8af5b9ed82
gatecat <gatecat@ds0.me>
2022-01-12 15:15:14 +0000

    Add post-pnr timing analysis
    Signed-off-by: gatecat <gatecat@ds0.me>

--- sta/openrcx.tcl
+++ sta/openrcx.tcl
@@ -1,4 +1,6 @@
 read_lef export/StdCellLib.lef
 read_def export/corona_cts_export.def
 extract_parasitics -ext_model_file ../../open_pdks/sky130/sky130A/libs.tech/openlane/rcx_rules.info
+write_verilog export/corona_cts_export.v
 write_spef export/corona_cts_export.spef
+exit
