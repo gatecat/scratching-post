c77b9c3f1e0ab97a5b722a79c028e79e25eefbca
gatecat <gatecat@ds0.me>
2022-02-04 19:14:06 +0000

    Init SPI flash
    Signed-off-by: gatecat <gatecat@ds0.me>

--- soc-ulx3s/sim/main.cc
+++ soc-ulx3s/sim/main.cc
@@ -3,7 +3,6 @@
 #include <backends/cxxrtl/cxxrtl.h>
 #include "build/sim_soc.h"
 #include "models/spiflash.h"
-#include "models/wb_mon.h"
 
 #include "models/log.h"
 
@@ -14,13 +13,14 @@ using namespace cxxrtl_design;
 int main(int argc, char **argv) {
     cxxrtl_design::p_sim__top top;
 
+    spiflash_load(*top.cell_p_flash, "../../vex-soc/software/bios.bin", 0x00100000U);
+
     top.step();
     auto tick = [&]() {
         top.p_clk.set(false);
         top.step();
         top.p_clk.set(true);
         top.step();
-        trace.tick();
     };
     top.p_rst.set(true);
     tick();
