124bee3f81bdb1a52df642a6c8fccd19dd34978a
gatecat <gatecat@ds0.me>
2022-03-09 21:14:57 +0000

    Working on upacket fsm
    Signed-off-by: gatecat <gatecat@ds0.me>

--- dp/dp_aux_sink/dp_aux_upacket.py
+++ dp/dp_aux_sink/dp_aux_upacket.py
@@ -22,6 +22,7 @@ class DPAuxPacketHandler(Elaboratable):
         self.reg_r_data  = Signal(8)
         self.reg_w_stb   = Signal()
         self.reg_r_stb   = Signal()
+        self.reg_adr_vld = Signal()
 
         # I2C interface
         self.i2c_active  = Signal()
@@ -48,15 +49,18 @@ class DPAuxPacketHandler(Elaboratable):
 
         nat_reply = Signal(2)
         i2c_reply = Signal(2)
-        reply_hdr = Cat(nat_reply, i2c_reply, C(0, 4))
+        reply_hdr = Cat(C(0, 4), i2c_reply, nat_reply)
 
-        byte_count = Signal(5)
+        rx_byte_count = Signal(5)
+        delay_count = Signal(5)
         length = Signal(8)
+        sent_reply_hdr = Signal()
+
         with m.If(self.rx_idle_in):
-            m.d.sync += byte_count.eq(0)
+            m.d.sync += rx_byte_count.eq(0)
         with m.Elif(self.rx_strobe):
             m.d.sync += [
-                byte_count.eq(byte_count + 1),
+                rx_byte_count.eq(rx_byte_count + 1),
             ]
 
         m.d.sync += [
@@ -74,22 +78,22 @@ class DPAuxPacketHandler(Elaboratable):
                 with m.If(self.rx_idle_in):
                     m.next = "IDLE"
                 with m.Elif(self.rx_stop_in):
-                    m.d.sync += byte_count.eq(0)
+                    m.d.sync += rx_byte_count.eq(0)
                     m.next = "RX_HEADER"
             with m.State("RX_HEADER"):
                 with m.If(self.rx_idle_in):
                     m.next = "IDLE"
                 with m.If(self.rx_strobe):
                     m.d.sync += header.eq(Cat(self.rx_data_in, header[:-8]))
-                    with m.If(byte_count == 2):
+                    with m.If(rx_byte_count == 2):
                         m.next = "GET_LENGTH"
-                        m.d.sync += byte_count.eq(0)
+                        m.d.sync += rx_byte_count.eq(0)
             with m.State("GET_LENGTH"):
                 # Update address latch from header
                 with m.If(is_i2c):
                     pass # TODO
                 with m.Else():
-                    self.reg_adr.eq(addr)
+                    m.d.sync += self.reg_adr.eq(addr)
                 # Determine next state
                 with m.If(self.rx_idle_in): # aborted early
                     m.next = "IDLE"
@@ -99,11 +103,11 @@ class DPAuxPacketHandler(Elaboratable):
                 with m.Elif(self.rx_strobe): # length field
                     m.d.sync += [
                         length.eq(self.rx_data_in),
-                        byte_count.eq(0),
+                        rx_byte_count.eq(0),
                     ]
                     m.next = "GOT_LENGTH"
             with m.State("GOT_LENGTH"):
-                with m.If((req == REQ_WSUR) | (req == REQ_RD) | (length == 0)):
+                with m.If((req == REQ_WSUR) | (req == REQ_RD) | (length == 0)): # no payload
                     with m.If(self.rx_idle_in):
                         m.next = "BEGIN_REPLY"
                 with m.Else():
@@ -115,10 +119,63 @@ class DPAuxPacketHandler(Elaboratable):
                     with m.If(is_i2c):
                         pass # TODO
                     with m.Else():
-                        self.reg_w_data.eq(self.rx_data_in)
-                        self.reg_w_stb.eq(1)
+                        m.d.sync += [
+                            self.reg_w_data.eq(self.rx_data_in),
+                            self.reg_w_stb.eq(1),
+                        ]
             with m.State("BEGIN_REPLY"):
-                pass
+                m.d.sync += [
+                    delay_count.eq(0),
+                    nat_reply.eq(0b00), # ACK
+                    i2c_reply.eq(0b00), # ACK
+                ]
+                with m.If(req == REQ_WR):
+                    m.d.sync += length.eq(0) # no payload to a write reply
+                with m.If(is_i2c & ~self.i2c_ack):
+                    m.d.sync += [
+                        i2c_reply.eq(0b01), # NACK
+                        length.eq(0),
+                    ]
+                with m.Elif(~is_i2c & ~self.reg_adr_vld):
+                    m.d.sync += [
+                        nat_reply.eq(0b01), # NACK
+                        length.eq(0),
+                    ]
+                m.next = "WAIT_BTA"
+            with m.State("WAIT_BTA"):
+                with m.If(delay_count.all()):
+                    # begin transmit
+                    m.d.sync += [
+                        self.tx_valid.eq(1),
+                        self.tx_data_out.eq(reply_hdr),
+                        self.tx_begin.eq(1),
+                        sent_reply_hdr.eq(0),
+                    ]
+                    with m.If(self.tx_busy):
+                        m.next = "SEND_RESP"
+                with m.Else():
+                    m.d.sync += delay_count.eq(delay_count + 1)
+            with m.State("SEND_RESP"):
+                with m.If(self.tx_ack): # first byte or byte ready to send
+                    with m.If(length == 0): # nothing more to send
+                        m.d.sync += self.tx_valid.eq(0)
+                        m.next = "RESP_DONE"
+                    with m.Else():
+                        with m.If(is_i2c):
+                            m.d.sync += self.i2c_r_stb.eq(1)
+                        with m.Else():
+                            m.d.sync += self.reg_r_stb.eq(1)
+                        m.d.sync += length.eq(length - 1)
+                    m.d.sync += sent_reply_hdr.eq(1)
+                with m.If(sent_reply_hdr): # route reply data
+                    with m.If(is_i2c):
+                        m.d.sync += self.tx_data_out.eq(self.i2c_r_data)
+                    with m.Else():
+                        m.d.sync += self.tx_data_out.eq(self.reg_r_data)
+                m.d.sync += self.tx_begin.eq(0)
+            with m.State("RESP_DONE"):
+                with m.If(~self.tx_busy):
+                    m.next = "IDLE"
 
         # Increment address latch one cycle later
         with m.If(self.reg_w_stb | self.reg_r_stb):
@@ -166,6 +223,7 @@ def sim_reg_read():
         for x in stop(): yield x
         for i in range(10): yield
         yield ph.rx_idle_in.eq(1)
+        for i in range(10): yield
 
     sim.add_sync_process(process)
     with sim.write_vcd("upacket.vcd", "upacket.gtkw"):
