b1e0f9e602e0c484f8e8c140b5208e8e3ac3bb05
gatecat <gatecat@ds0.me>
2022-08-17 13:04:10 +0200

    Setting out the basic primitive library
    Signed-off-by: gatecat <gatecat@ds0.me>

---
+++ clb-gen/tech/generic.v
@@ -0,0 +1,54 @@
+module clb_mux2(input a0, a1, s0, output x);
+	assign x = s0 ? a1 : a0;
+endmodule
+
+module clb_mux4(input a0, a1, a2, a3, s0, s1, output x);
+	assign x = s1 ? (s0 ? a3 : a2) : (s0 ? a1 : a0);
+endmodule
+
+module clb_buf(input a, output x);
+	assign x = a;
+endmodule
+
+module clb_not(input a, output x);
+	assign x = ~a;
+endmodule
+
+module clb_and(input a, b, output x);
+	assign x = a & b;
+endmodule
+
+module clb_or(input a, b, output x);
+	assign x = a | b;
+endmodule
+
+module clb_xor(input a, b, output x);
+	assign x = a ^ b;
+endmodule
+
+module clb_nand(input a, b, output x);
+	assign x = ~(a & b);
+endmodule
+
+module clb_nor(input a, b, output x);
+	assign x = ~(a | b);
+endmodule
+
+module clb_xnor(input a, b, output x);
+	assign x = ~(a ^ b);
+endmodule
+
+module clb_dff(input clk, d, output reg q);
+	always @(posedge clk)
+		q <= d;
+endmodule
+
+module clb_dffrs(input clk, d, rn, sn, output reg q);
+	always @(posedge clk, negedge rn, negedge sn)
+		if (!rn)
+			q <= 0;
+		else if (!sn)
+			q <= 1;
+		else
+			q <= d;
+endmodule
