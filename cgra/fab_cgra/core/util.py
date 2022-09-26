b29889fadfa9633a5a6bbf9b3b565a3aa35f7553
gatecat <gatecat@ds0.me>
2022-09-26 13:41:23 +0200

    types
    Signed-off-by: gatecat <gatecat@ds0.me>

---
+++ cgra/fab_cgra/core/util.py
@@ -0,0 +1,6 @@
+from amaranth import *
+from typing import TypeAlias
+
+__all__ = ["PortVal"]
+
+PortVal: TypeAlias = Value|int
--- cgra/fab_cgra/tech/base.py
+++ cgra/fab_cgra/tech/base.py
@@ -1,23 +1,37 @@
 from amaranth import *
+from ..core.util import *
+
+__all__ = ["BaseTech"]
 
 class BaseTech:
-	def __init__(self, m):
+	def __init__(self, m: Module):
 		self.m = m
 		self.autoidx = 0
 
-	def _add_submod(self, name, inst):
+	def _add_submod(self, name: str, inst: Instance):
 		if name is None:
 			name = f'i{self.autoidx}'
 			self.autoidx += 1
 		assert not hasattr(self.submodules, name)
 		setattr(self.submodules, name, inst)
 
-	def add_gate(self, type, name=None, **ports):
-		inst = Instance(typ,
+	def add_gate(self, typ: str, name: str|None=None, **ports: dict[str, PortVal]):
+		# TODO: gate golfing
+		assert name in ("and2", "nand2", "or2", "nor2", "xor2", "xnor2",
+			"mux2", "mux4", "mux8", "mux16", "buf", "clkbuf")
+		inst = Instance(f"generic_{typ}",
 			**{f'{"o" if p in ("x", "y", "q") else "i"}_{p}': q for p, q in ports}
 		)
 		self._add_submod(name, inst)
 
+	def avail_mux_sizes():
+		return (2, 4, 8, 16)
 
-
+	def add_mux(self, inputs: list[PortVal], sel: list[PortVal], y: Signal, name=None):
+		assert len(inputs) in self.avail_mux_sizes()
+		assert len(sel) == len(inputs).bit_length()
+		add_gate(type=f"mux{len(inputs)}", y=y, 
+			**{f"i{i}": s for i, s in enumerate(inputs)},
+			**{f"s{i}": s for i, s in enumerate(sel)}
+		)
 
