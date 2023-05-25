042e3960bce59d2817885d77a6581abef8116a5a
gatecat <gatecat@ds0.me>
2023-05-25 14:37:45 +0200

    explore dcst/dsct
    Signed-off-by: gatecat <gatecat@ds0.me>

--- eplc/fft_test.py
+++ eplc/fft_test.py
@@ -36,7 +36,7 @@ for y, row in enumerate(in_density):
 		density[y0,x1] += d * (x1/xs - x)*xs * (y - y0/ys)*ys
 		density[y1,x1] += d * (x1/xs - x)*xs * (y1/ys - y)*ys
 
-a = fft.dctn(density, type=2)
+a = fft.dctn(density, type=1)
 for v in range(m):
 	for u in range(m):
 		# differing scale factors
@@ -50,13 +50,37 @@ for v in range(m):
 		if v != 0 or u != 0:
 			potential[v,u] = a[v,u] * (1.0 / (w_u**2 + w_v**2))
 
-potential = fft.dctn(potential, type=2)
+potential = fft.dctn(potential, type=1)
 
 fig, axs = plt.subplots(2, 2)
 axs[0,0].imshow(density, cmap='hot', interpolation='nearest')
 axs[0,0].set_title("density")
 
-axs[0,1].imshow(potential, cmap='hot', interpolation='nearest')
-axs[0,0].set_title("potential")
+axs[0,1].imshow(potential, cmap='coolwarm', interpolation='nearest')
+axs[0,1].set_title("potential")
+
+x_field = np.array([[0] * m] * m)
+y_field = np.array([[0] * m] * m)
+for v in range(m):
+	for u in range(m):
+		w_u = (2*pi*u)/m
+		w_v = (2*pi*v)/m
+		if v != 0 or u != 0:
+			x_field[v,u] = a[v,u] * (w_u / (w_u**2 + w_v**2))
+			y_field[v,u] = a[v,u] * (w_v / (w_u**2 + w_v**2))
+
+# dcst
+x_field = fft.dst(x_field, axis=0, type=1)
+x_field = fft.dct(x_field, axis=1, type=1)
+
+axs[1,0].imshow(x_field, cmap='coolwarm', interpolation='nearest')
+axs[1,0].set_title("field X component")
+
+# dsct
+y_field = fft.dct(y_field, axis=0, type=1)
+y_field = fft.dst(y_field, axis=1, type=1)
+
+axs[1,1].imshow(y_field, cmap='coolwarm', interpolation='nearest')
+axs[1,1].set_title("field Y component")
 
 plt.show()
