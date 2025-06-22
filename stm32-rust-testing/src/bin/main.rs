fa538c4e25bf3fef5548c4697f1cbd4d037186ca
gatecat <gatecat@ds0.me>
2025-06-22 16:29:02 +0200

    servo
    Signed-off-by: gatecat <gatecat@ds0.me>

--- stm32-rust-testing/src/bin/main.rs
+++ stm32-rust-testing/src/bin/main.rs
@@ -4,7 +4,12 @@
 use defmt::info;
 use embassy_executor::Spawner;
 use embassy_stm32::Config;
-use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_stm32::gpio::{Level, Output, Speed, OutputType};
+
+use embassy_stm32::timer::simple_pwm::{PwmPin, SimplePwm};
+use embassy_stm32::timer::low_level::CountingMode;
+use embassy_stm32::time::hz;
+
 use embassy_time::Timer;
 use {defmt_rtt as _, panic_probe as _};
 
@@ -15,11 +20,25 @@ async fn main(_spawner: Spawner) -> ! {
 
     let mut led = Output::new(p.PB7, Level::High, Speed::Low);
 
+    led.set_high();
+
+    let servo_pin = PwmPin::new_ch4(p.PA3, OutputType::PushPull);
+    let mut pwm = SimplePwm::new(p.TIM5, None, None, None, Some(servo_pin), hz(50), CountingMode::EdgeAlignedUp);
+
+    let max_duty = pwm.max_duty_cycle();
+
+    let servo_max = max_duty / 10;
+    let servo_min = max_duty / 22;
+
+    let mut ch4 = pwm.ch4();
+    ch4.set_duty_cycle(servo_min);
+    ch4.enable();
     loop {
         info!("Hello World!");
         led.set_high();
-        Timer::after_millis(500).await;
-        led.set_low();
-        Timer::after_millis(500).await;
+        for i in 0..5 {
+            ch4.set_duty_cycle(servo_min + ((i * ((servo_max - servo_min) as u32)) / 5) as u16);
+            Timer::after_millis(1000).await;
+        }
     }
 }
