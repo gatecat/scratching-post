ec92e35c2cf917ad0db81322e25c1188913ef87e
gatecat <gatecat@ds0.me>
2025-06-30 12:33:29 +0200

    skeleton
    Signed-off-by: gatecat <gatecat@ds0.me>

---
+++ amaranth-axi-soc/axilite/bus.py
@@ -0,0 +1,32 @@
+from amaranth import *
+from amaranth.lib import data, stream, wiring
+from amaranth.lib.wiring import In, Out
+
+class Protection(data.Struct):
+    privileged:  1
+    non_secure:  1
+    instruction: 1
+
+
+class Signature(wiring.Signature):
+    def aw_signature(self):
+        return stream.Signature(data.StructLayout({
+            "addr": self._addr_width,
+            "prot": Protection,
+        }))
+
+    def __init__(self, *, addr_width, data_width):
+        if not isinstance(addr_width, int) or addr_width < 0:
+            raise TypeError(f"Address width must be a non-negative integer, not {addr_width!r}")
+        if data_width not in (8, 16, 32, 64):
+            raise ValueError(f"Data width must be one of 8, 16, 32, 64, not {data_width!r}")
+
+        self._addr_width  = addr_width
+        self._data_width  = data_width
+
+        super().__init__({
+            "aw": Out(self.aw_signature())
+        })
+
+if __name__ == '__main__':
+    Signature(addr_width=32, data_width=32)
