431e13b38ed62e6d6c09b57507542a4c9c78170b
gatecat <gatecat@ds0.me>
2025-09-02 09:39:48 +0200

    wip: verilator
    Signed-off-by: gatecat <gatecat@ds0.me>

---
+++ example-soc-postsyn/main.cc
@@ -0,0 +1,61 @@
+#undef NDEBUG
+
+#include "verilated.h"
+#include "Vsim_top.h"
+#include "models/models.h"
+
+#include <fstream>
+#include <filesystem>
+
+int main(int argc, char **argv) {
+    VerilatedContext* contextp = new VerilatedContext;
+    contextp->commandArgs(argc, argv);
+
+    Vsim_top *top = new Vsim_top{contextp};
+
+    spiflash_model flash("flash", &top->soc_flash_clk, &top->soc_flash_csn,
+        &top->soc_flash_d_o, &top->soc_flash_d_oe, &top->soc_flash_d_i);
+
+    uart_model uart_0("uart_0", &top->soc_uart_0_tx, &top->soc_uart_0_rx);
+
+    gpio_model gpio_0("gpio_0", &top->soc_gpio_0_gpio_o, &top->soc_gpio_0_gpio_oe, &top->soc_gpio_0_gpio_i);
+
+    open_event_log("events.json");
+    open_input_commands("input.json");
+
+    unsigned timestamp = 0;
+    auto tick = [&]() {
+        // agent.print(stringf("timestamp %d\n", timestamp), CXXRTL_LOCATION);
+
+        flash.step(timestamp);
+        uart_0.step(timestamp);
+
+        gpio_0.step(timestamp);
+
+        top->clk = 0;
+        top->eval();
+        ++timestamp;
+
+        top->clk = 1;
+        top->eval();
+        ++timestamp;
+
+        // if (timestamp == 10)
+        //     agent.breakpoint(CXXRTL_LOCATION);
+    };
+
+    flash.load_data("software.bin", 0x00100000U);
+
+
+    top->rst_n = 0;
+    tick();
+
+    top->rst_n = 1;
+    for (int i = 0; i < 3000000; i++)
+        tick();
+
+    close_event_log();
+    delete top;
+    delete contextp;
+    return 0;
+}
--- example-soc-postsyn/verilator_wrapper.v
+++ example-soc-postsyn/verilator_wrapper.v
@@ -10,13 +10,13 @@ module sim_top(
 	output [3:0] soc_flash_d_oe,
 	input [3:0] soc_flash_d_i,
 
-	output [7:0] gpio_0_gpio_o,
-	output [7:0] gpio_0_gpio_oe,
-	input [7:0] gpio_0_gpio_i,
+	output [7:0] soc_gpio_0_gpio_o,
+	output [7:0] soc_gpio_0_gpio_oe,
+	input [7:0] soc_gpio_0_gpio_i,
 
-	output [3:0] gpio_open_drain_gpio_o,
-	output [3:0] gpio_open_drain_gpio_oe,
-	input [3:0] gpio_open_drain_gpio_i,
+	output [3:0] soc_gpio_open_drain_gpio_o,
+	output [3:0] soc_gpio_open_drain_gpio_oe,
+	input [3:0] soc_gpio_open_drain_gpio_i,
 
 	input soc_uart_0_rx,
 	output soc_uart_0_tx
@@ -39,13 +39,13 @@ module sim_top(
 	assign soc_flash_d_oe = ~gpio_oeb[5:2];
 	assign gpio_in[5:2] = soc_flash_d_i;
 
-	assign gpio_0_gpio_o = gpio_out[15:8];
-	assign gpio_0_gpio_oe = ~gpio_oeb[15:8];
-	assign gpio_in[15:8] = gpio_0_gpio_i;
+	assign soc_gpio_0_gpio_o = gpio_out[15:8];
+	assign soc_gpio_0_gpio_oe = ~gpio_oeb[15:8];
+	assign gpio_in[15:8] = soc_gpio_0_gpio_i;
 
-	assign gpio_open_drain_gpio_o = gpio_out[28:25];
-	assign gpio_open_drain_gpio_oe = ~gpio_oeb[28:25];
-	assign gpio_in[28:25] = gpio_open_drain_gpio_i;
+	assign soc_gpio_open_drain_gpio_o = gpio_out[28:25];
+	assign soc_gpio_open_drain_gpio_oe = ~gpio_oeb[28:25];
+	assign gpio_in[28:25] = soc_gpio_open_drain_gpio_i;
 
 	assign gpio_in[7] = soc_uart_0_rx;
 	assign soc_uart_0_tx = gpio_in[6];
